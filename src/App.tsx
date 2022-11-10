f41c0e43ac8c47ab5fe70aa4874fd95ff6c083db
ssar_say <sayhoo.saryglar@mail.ru>
2022-11-10 18:52:42 +0700

    change to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Store } from "./components/Store";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { TradesStore } from "./components/TradesStore";
 
 function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<Store/>}/>
                 <Route path="/trades" element={<TradesStore/>}/>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
